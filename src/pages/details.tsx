c46f59f3420b73c3b7b4133338909c97c8fea62c
Daniel Brito <dlmf@gft.com>
2023-11-03 21:36:11 -0300

    second

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,27 +1,27 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import React from 'react';
 
-const Details: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>Detalhes</title>
-                <meta name="description" content="Página de detalhes" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+import SEO from '@/components/atoms/SEO';
 
-            <main className="flex flex-col items-center justify-center min-h-screen py-2">
-                <h1 className="text-6xl font-bold">
-                    Bem-vindo à <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-
-                {/* Conteúdo da sua página de detalhes */}
-                <p className="mt-3 text-2xl">
-                    Esta é a página de detalhes.
-                </p>
-            </main>
-        </>
-    );
+const seoInfo = {
+  title: 'LoL | Home',
+  url: 'http://localhost:3000/',
+  description: 'website description',
+  siteName: 'Leadership for Executives',
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
 };
 
-export default Details;
+const registration = () => {
+  return (
+    <>
+      <SEO SEOinformation={seoInfo} />
+      <span>Details</span>
+    </>
+  );
+};
+
+export default registration;
+
